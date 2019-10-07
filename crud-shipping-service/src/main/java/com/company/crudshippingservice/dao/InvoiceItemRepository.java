2eaaa3c8e6e2dd24664ac224f3677f3db7f722ee
fara9084 <lzda.angel@gmail.com>
2019-10-07 11:43:07 -0400

    Test for JPA

--- crud-shipping-service/src/main/java/com/company/crudshippingservice/dao/InvoiceItemRepository.java
+++ crud-shipping-service/src/main/java/com/company/crudshippingservice/dao/InvoiceItemRepository.java
@@ -1,4 +1,10 @@
 package com.company.crudshippingservice.dao;
 
-public interface InvoiceItemRepository {
+import com.company.crudshippingservice.model.InvoiceItem;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface InvoiceItemRepository extends JpaRepository<InvoiceItem,Integer> {
+
 }
--- crud-shipping-service/src/main/java/com/company/crudshippingservice/dao/InvoiceRepository.java
+++ crud-shipping-service/src/main/java/com/company/crudshippingservice/dao/InvoiceRepository.java
@@ -1,5 +1,12 @@
 package com.company.crudshippingservice.dao;
 
-public interface InvoiceRepository{
+import com.company.crudshippingservice.model.Invoice;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
+@Repository
+public interface InvoiceRepository extends JpaRepository<Invoice,Integer> {
+    List<Invoice> getInvoicesByCustomerId(int customerId);
 }
